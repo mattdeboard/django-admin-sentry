b862a6300376414c24e379487714eb788d324533
Matt DeBoard <matt.deboard@gmail.com>
2011-07-15 18:07:17 +0200

    Added caching to views.

--- views.py
+++ views.py
@@ -1,19 +1,22 @@
+
+
 from django.contrib import messages
 from django.contrib.admin.models import LogEntry
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.core.cache import cache
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from admin_sentry import settings
 
 ACTIONS = {1: 'Addition', 2: 'Change', 3: 'Deletion'}
-
+MINUTES_TO_CACHE = 60
 
 @login_required
 def index(request):
     qs = LogEntry.objects.all().order_by('-action_time')
-    users = User.objects.all()
+    users = cache_users()
     return render_to_response('admin_sentry/index.html', 
                               {'results':qs, 'userlist': users,
                                'changes':ACTIONS.iterkeys()},
@@ -21,8 +24,29 @@ def index(request):
 
 @login_required
 def by_user(request, loguser):
-    qs = LogEntry.objects.filter(user__username=loguser)
+    qs = get_user_logs(loguser)
+    users = cache_users()
     return render_to_response('admin_sentry/user.html', {'results':qs,
-                                                         'loguser':loguser},
+                                                         'loguser':loguser,
+                                                         'userlist':users},
                               context_instance=RequestContext(request))
 
+def get_user_logs(user):
+    cache_key = 'adminlog:%s-logs' % user
+    results = cache.get(cache_key)
+
+    if not results:
+        results = LogEntry.objects.filter(user__username=user)
+        cache.set(cache_key, results, MINUTES_TO_CACHE * 5)
+
+    return results
+
+def cache_users():
+    cache_key = 'adminlog:allusers'
+    users = cache.get(cache_key)
+
+    if not users:
+        users = User.objects.all()
+        cache.set(cache_key, users, MINUTES_TO_CACHE * 60)
+
+    return users
