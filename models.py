57181d46fb2b0e7873a6ca578241312f57ef82fd
Matt DeBoard <matt.deboard@gmail.com>
2011-07-18 21:02:21 +0200

    Wrote up model to keep track of changes one revision deep.

--- models.py
+++ models.py
@@ -8,33 +8,11 @@ from django.utils.encoding import smart_unicode
 from django.utils.safestring import mark_safe
 
 
-class LogProxy(object):
+class ChangeLog(models.Model):
     '''
-    Represents a LogEntry instance without needing to rewrite the core 
-    LogEntry model. Instantiate with the id of a LogEntry instance, e.g.:
-
-        my_entry = LogProxy(3)
-
-    All attributes & methods of the LogEntry with id==3 will be accessible
-    via e.g.:
-        
-        my_entry.entry.user | my_entry.object_repr
-
-    etc.
     '''
-        
-    def get_action(self):
-        # Values for this dict pulled from django.contrib.admin.models.LogEntry
-        actions = {0: 'Addition', 1: 'Change', 2: 'Deletion'}
-        return actions[self.entry.action_flag]
-
-
-class FilterValue(models.Model):
-    FILTER_KEYS = (
-            ('user', _('server name')),
-            ('action', _('action')),
-            ('object', _('object')),
-    )
-
-    key = models.CharField(choices=FILTER_KEYS, max_length=32)
-    value = models.CharField(max_length=200)
+    prev_state = {}
+    diff_pairs = []
+    def __init__(self, content_type=None, ref_id=None):
+        self.content_type = content_type
+        self.ref_id = ref_id
