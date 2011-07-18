e220a85ed4d01fd91f03af35e1e9d2b49f7fb742
Matt DeBoard <matt.deboard@gmail.com>
2011-07-18 22:52:21 +0200

    Removed ChangeLog model.

--- models.py
+++ models.py
@@ -7,24 +7,5 @@ from django.utils.translation import ugettext_lazy as _
 from django.utils.encoding import smart_unicode
 from django.utils.safestring import mark_safe
 
-from directseo.seo.models import Configuration
-
 import admin_sentry.diff_match_patch as diff
-
-
-class ChangeLog(models.Model):
-    '''
-    '''
-    ref_id = models.IntegerField()
-    model_class = models.CharField(max_length=255)
-    
-    def set_content_type(self, content_type=None):
-        self.content_type = content_type
-
-    def get_content_type(self):
-        return self.content_type
-        
-    def get_html_diff(self):
-        return 
-        
         
