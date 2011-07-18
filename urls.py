acc3ad28931e4c7dc15d14a47109c893a244acfe
Matt DeBoard <matt.deboard@gmail.com>
2011-07-18 16:06:11 +0200

    Removed user/ and change_type/ url patterns since inline filters will be used for both.

--- urls.py
+++ urls.py
@@ -11,8 +11,4 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='admin_sentry'),
     url(r'^_static/(?P<path>.*)$', 'django.views.static.serve', 
         {'document_root':STATIC_ROOT},  name="as-media"),
-    url(r'^user/(?P<loguser>.*)$', views.by_user, name='user-view'),
-    url(r'^changes/(?P<action>addition|update|deletion)$', views.by_changetype,
-        name='change-view'),
-
 )
