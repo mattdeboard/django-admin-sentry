3d64687447f49efdd6612430f6d6d857ea8cfa06
Matt DeBoard <matt.deboard@gmail.com>
2011-07-19 22:39:49 +0200

    UserFilter will no longer include users who have no log entries to report, and only exist to clutter up the UI.

--- admin_sentry/filters.py
+++ admin_sentry/filters.py
@@ -107,7 +107,7 @@ class UserFilter(BaseFilter):
     column = 'user'
     
     def get_choices(self):
-        userdict = SortedDict([(user.id, user.username) for user in cache_users()])
+        userdict = SortedDict([(user.id, user.username) for user in cache_users() if user.logentry_set.count()])
         return userdict
 
 
