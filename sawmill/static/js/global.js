d315163613010dfea2022c65b34a599f783a531f
Matt DeBoard <matt.deboard@gmail.com>
2011-07-22 16:02:09 +0200

    Added click & hover listeners to row objects

---
+++ sawmill/static/js/global.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  $("li.row").click(function () {
+    var model = this.getAttribute("model_id");
+    var obj_id = this.getAttribute("obj_id");
+    window.location.href = "/sawmill/activity/" + model + "/" + obj_id;
+  });
+  
+  $("li.row").hover(
+    function () {
+      $("body").css("cursor", "pointer");
+    },
+    
+    function () {
+      $("body").css("cursor", "default");
+    });
+});
+
+
