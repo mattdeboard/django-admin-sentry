1ba1f9d4c5bca452e3b247b2ea460f4643a6884b
Matt DeBoard <matt.deboard@gmail.com>
2011-07-28 21:22:13 +0200

    Added content type select box.

--- sawmill/forms.py
+++ sawmill/forms.py
@@ -1,7 +1,25 @@
 from django import forms
 
+CONTENT_TYPES = (
+    (2, "Groups"),
+    (3, "Users"),
+    (7, "Sites"),
+    (14, "Indices"),
+    (15, "Social Links"),
+    (19, "Configurations"),
+    (20, "SEO Sites"),
+    (21, "SEO Site Redirects"),
+    (23, "Google Analytics"),
+    (25, "Social Links"),
+)
+
+
 class ASLogin(forms.Form):
     username = forms.CharField(max_length=100)
     password = forms.CharField(max_length=30,
                                widget=forms.PasswordInput(render_value=False))
+
+class ContentDropDown(forms.Form):
+    content_type = forms.CharField(max_length=25,
+                                   widget=forms.Select(choices=CONTENT_TYPES))
     
