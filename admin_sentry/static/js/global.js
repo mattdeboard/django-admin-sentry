450993f763336e4d758d5785b40e561de053a942
Matt DeBoard <matt.deboard@gmail.com>
2011-08-12 16:22:37 +0200

    More name change cleanup

--- admin_sentry/static/js/global.js
+++ admin_sentry/static/js/global.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   $("li.row").click(function () {
     var model = this.getAttribute("model_id");
     var obj_id = this.getAttribute("obj_id");
-    window.location.href = "/sawmill/activity?&model=" + model + "&obj=" + obj_id;
+    window.location.href = "/admin_sentry/activity?&model=" + model + "&obj=" + obj_id;
   });
   
   $("li.row").hover(
@@ -25,7 +25,7 @@ $(document).ready(function () {
   
   $(".sidebar-module").delegate("select#id_content_type","change",function () {
     console.log(this.value);
-    window.location.href = "/sawmill/activity?&model=" + this.value + "&obj=";
+    window.location.href = "/admin_sentry/activity?&model=" + this.value + "&obj=";
   });
 });
 
